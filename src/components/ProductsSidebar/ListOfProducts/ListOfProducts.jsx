015139658dedd0c2dc62441e12f0e5c568a51a24
pivanrios <pipr96@gmail.com>
2024-05-24 03:53:37 -0700

    sidebar for layout

--- src/components/ProductsSidebar/ListOfProducts/ListOfProducts.jsx
+++ src/components/ProductsSidebar/ListOfProducts/ListOfProducts.jsx
@@ -1,20 +1,23 @@
 //functions
 import { getDishes } from '@/src/lib/crud'
+//components
 import Delete from './Buttons/Delete';
+import Link from 'next/link';
 
 async function ListOfProducts() {
     //our states
     const dishes = await getDishes();
 
     return (
-        <div className='w-full flex flex-col gap-1 bg-slate-700'>
+        <div className='w-full flex flex-col border-solid border-black gap-1'>
             {dishes.map((dish)=>(
-                <span className=' text-white bg-slate-500 flex flex-row w-full justify-between'
+                <Link   href={`/admin/products/${dish.name}`}
+                        className=' text-white flex flex-row w-full justify-between cursor-pointer 
+                px-2 '
                         key={dish.name}>
                     <p>{dish.name}</p>
-                    <p>{dish.price}</p>
                     <Delete/>
-                </span>
+                </Link>
             ))}
         </div>
     )
