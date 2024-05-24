343f5ca74806057259b0a3fc5e3d60612bff870e
pivanrios <pipr96@gmail.com>
2024-05-24 15:48:09 -0700

    add home link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="">
-        <nav className=" bg-slate-200 gap-2 flex flex-row justify-center">
+    <header className="flex flex-row justify-between bg-slate-400">
+      <div/>
+        <nav className=" gap-2 flex flex-row justify-center">
             <Link href={"/admin"} className="">Orders</Link>
             <Link href={"/admin/products"}>Products</Link>
             <Link href={"/admin/customers"}>Customers</Link>
             <Link href={"/admin/sales"}>Sales</Link>
         </nav>
+        <Link href={"/"}>back</Link>
     </header>
   )
 }
