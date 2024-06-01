414299113003badff3ef52ad5c7c1b98ce40e178
pivanrios <pipr96@gmail.com>
2024-06-01 02:01:22 -0700

    add data

---
+++ public/data.js
@@ -0,0 +1,45 @@
+export const orders = [
+    {
+        id: 1,
+        name: "Pedro Perez",
+        product: "Lasagna",
+        qty: 1,
+        paid: true,
+        total: 9
+    },{
+        id: 2,
+        name: "Pedro Perez",
+        product: "Lasagna",
+        qty: 1,
+        paid: true,
+        total: 9
+    },{
+        id: 3,
+        name: "Pedro Perez",
+        product: "Lasagna",
+        qty: 1,
+        paid: true,
+        total: 9
+    },{
+        id: 4,
+        name: "Pedro Perez",
+        product: "Lasagna",
+        qty: 1,
+        paid: true,
+        total: 9
+    }
+]
+
+export const reviews = [
+    {
+        author: "peter",
+        rate: 1,
+        content: "very good"
+    }
+]
+export const dishes = [
+    {
+        name: "lasagna",
+        price: 9
+    }
+]
