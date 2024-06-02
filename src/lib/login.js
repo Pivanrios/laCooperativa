c79cf4c1652c6121d3fafb083e9b9e12718be587
pivanrios <pipr96@gmail.com>
2024-06-01 20:11:06 -0700

    auth functions

---
+++ src/lib/login.js
@@ -0,0 +1,7 @@
+export function login(formData){
+    try {
+        
+    } catch (error) {
+        console.error(error);
+    }
+};
--- src/lib/signup.js
+++ src/lib/signup.js
@@ -10,7 +10,7 @@ export async function signUpUser(formData){
     console.log("entries:", username, email, password, company, shift);
     try {
       const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(userCredentials);  
+      //console.log(userCredentials);  
     } catch (error) {
         console.error(error);
     }
