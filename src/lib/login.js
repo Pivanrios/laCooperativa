142ccb5727731783decf8adb32300a5aac91fe1d
pivanrios <pipr96@gmail.com>
2024-06-01 20:51:29 -0700

    signin with email

--- src/lib/login.js
+++ src/lib/login.js
@@ -1,6 +1,13 @@
-export function login(formData){
+import { auth } from "@/firebaseconfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export async function login(formData){
+    "use server"
+    const {email, password} = Object.fromEntries(formData);
+
     try {
-        
+        const credentials = await signInWithEmailAndPassword(auth, email, password);
+        //console.log("credenciales", credentials);
     } catch (error) {
         console.error(error);
     }
