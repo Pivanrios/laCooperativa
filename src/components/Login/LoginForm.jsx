8f5d9d8f9353c9ff0a224cd514475e11dc195527
pivanrios <pipr96@gmail.com>
2024-06-06 10:31:15 -0700

    getAccesstoken

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,13 +1,20 @@
-
+"use client"
 import { login } from "@/src/lib/login"
 
 function LoginForm() {
+  const handleSubmit = async (formData)=>{
+    
+    const user = await login(formData);
+    console.log("user",user);
+   
+  };
 
   return (
     <section className="flex flex-col items-center">
         <h2 className="text-2xl font-bold">Login</h2>
         <div>
-            <form action={login}
+            <form action={handleSubmit}
+
                     className="flex flex-col gap-1">
                 <label htmlFor="">Email:
                     <input type="text" name="email" id="username"
--- src/lib/login.js
+++ src/lib/login.js
@@ -1,14 +1,19 @@
+  "use server"
 import { auth } from "@/firebaseconfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 export async function login(formData){
-    "use server"
+    console.log(formData)
     const {email, password} = Object.fromEntries(formData);
+    let credentials = {}
 
     try {
-        const credentials = await signInWithEmailAndPassword(auth, email, password);
-        //console.log("credenciales", credentials);
+        console.log("sign in with email:", email)
+        const res = await signInWithEmailAndPassword(auth, email, password);
+        
+        console.log("credenciales", res.user.accessToken);
     } catch (error) {
         console.error(error);
     }
+    return credentials;
 };
