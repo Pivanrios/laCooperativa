d09c89b654eec55d18396b50757add2185ea16c7
pivanrios <pipr96@gmail.com>
2024-06-06 22:35:03 -0700

    resgistersTATUS

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,15 +1,27 @@
 'use client'
+import { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthContext"
+
 
 function RegisterForm() {
+  const {currentUser} = useAuth();
+  const [login, setLogin] = useState(false);
+  useEffect(()=>{
+    if(currentUser){
+      setLogin(true);
+    }else{
+      setLogin(false);
+    }
+  },[currentUser])
   return (
-    <section className='container flex flex-col items-center gap-1 bg-yellow-400 p-2' >
-      <div id='register-wrapper'
+    <section className='container flex flex-col items-center gap-1 ' >
+      {!login && <div id='register-wrapper'
           className='container flex flex-col items-center py-5 px-2'>
         <h3 className='text-2xl font-bold'>Register</h3>
         <p className=" "> Register with your email to receive cupons with discounts and add reviews to our dishes. </p>
       
         <button className='btn self-center'>Register</button>
-      </div>
+      </div>}
     </section>
   )
 }
