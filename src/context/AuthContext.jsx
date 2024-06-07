4d4a3672876a89b15485f10ab8ca5ac7d9155050
pivanrios <pipr96@gmail.com>
2024-06-06 22:05:07 -0700

    import function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,42 +2,40 @@
 // context/AuthContext.js
 import { createContext, useContext, useEffect, useState } from "react";
 import { auth } from "@/firebaseconfig";
-import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 //create context
 const AuthContext = createContext();
-
+//
 export function useAuth() {
   return useContext(AuthContext);
 }
-
+//
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+    const [currentUser, setCurrentUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    //
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+        setCurrentUser(user);
+        setLoading(false);
+        });
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      setCurrentUser(user);
-      setLoading(false);
-    });
-
-    return unsubscribe;
-  }, []);
-
-
-  const signIn = (email, password) => {
-    console.log("on signin function", email, password)
-    return signInWithEmailAndPassword(auth, email, password);
-  };
-
-  const value = {
-    currentUser,
-    signIn
-  };
-
-  return (
-    <AuthContext.Provider value={value}>
-      {!loading && children}
-    </AuthContext.Provider>
-  );
+        return unsubscribe;
+    }, []);
+    //
+    const signIn = (email, password) => {
+        console.log("on signin function", email, password)
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+    //
+    const value = {
+        currentUser,
+        signIn
+    };
+    return (
+        <AuthContext.Provider value={value}>
+        {!loading && children}
+        </AuthContext.Provider>
+    );
 }
