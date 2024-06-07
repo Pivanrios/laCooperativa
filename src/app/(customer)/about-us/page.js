940ec08914b4a74ffa8366f0951e948c140ae907
pivanrios <pipr96@gmail.com>
2024-06-06 23:12:37 -0700

    change mision and vision add images

--- src/app/(customer)/about-us/page.js
+++ src/app/(customer)/about-us/page.js
@@ -6,17 +6,34 @@ function About() {
         <section className='p-2'>
             <div className='flex  flex-col items-center px-3 py-5 md:px-10 bg-yellow-400 rounded-lg'>
                 <h3 className='text-3xl md:text-5xl font-bold'>Why?</h3>
-                <span className='flex gap-2'>
-                    <img src='../images/lasagnaCompleta.jpeg' alt='comida' width={200}/>
+                <span className='flex flex-col sm:flex-row gap-2 items-center'>
+                    <img src='/images/mision.webp' alt='comida' width={200}
+                        className='rounded-lg'/>
                     <p>
-                        No todos tenemos tiempo para preparar nuestro lonche todos los dias.
-                        La comida congelada dentro de las empresas suele ser la opcion mas saludable, 
-                        y mientras que los servicios de reparticion suelen ser caros.
-                        Abriendo un canal de comunicaion entre los companeros podemos ampliar nuestras
-                        opciones y crear un menu variado que satisfaga los gustos de todos.
+                    LaCooperativa abre un canal de comunicación entre compañeros de trabajo para compartir 
+                    y coordinar opciones de comidas saludables y accesibles. 
+                    A través de nuestra plataforma colaborativa, ampliamos las opciones culinarias 
+                    disponibles, permitiendo la creación de menús variados que satisfacen los gustos 
+                    y necesidades de todos, promoviendo una alimentación balanceada y comunitaria.
                     </p>
                 </span>
             </div> 
+        </section>
+        <section className='p-2'>
+        <div className='flex  flex-col items-center px-3 md:px-10 py-5 bg-yellow-400 rounded-lg'>
+                <h3 className='text-3xl md:text-5xl font-bold'>Vision</h3>
+                <span className='flex flex-col sm:flex-row  items-center gap-2'>
+                    <p>
+                    Buscamos ser una alternativa para la alimentación en el entorno laboral, 
+                    fomentando comunidades de trabajo saludables y conectadas. Aspiramos a transformar 
+                    la manera en que los empleados planifican y disfrutan sus comidas, creando un modelo 
+                    sostenible que mejore el bienestar en los lugares de trabajo. Además, buscamos 
+                    generar ingresos pasivos para empleados y sus cónyuges, permitiendo a las 
+                    amas de casa vender sus productos caseros.
+                    </p>
+                    <img src='/images/vision.webp' alt='comida' width={200} className=' rounded-lg'/>
+                </span>
+            </div>
         </section>
         <section className='p-2'>
             <div className='flex  flex-col items-center px-3 md:px-10 py-5 bg-yellow-400 rounded-lg'>
@@ -32,9 +49,6 @@ function About() {
                     <img src='../images/lasagnaCompleta.jpeg' alt='comida' width={200} className=' rounded-full'/>
                 </span>
             </div>
-        </section>
-        <section>
-            
         </section>
     </div>
   )
