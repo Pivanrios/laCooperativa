cc8ad96f151adef02637fde80de4bbcdd6946bb3
pivanrios <pipr96@gmail.com>
2024-06-07 11:04:33 -0700

    add register page

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -3,12 +3,15 @@
 import { useEffect, useState } from "react";
 //context
 import { useAuth } from "../context/AuthContext"
+//
+import { useRouter } from "next/navigation";
 
 function RegisterForm() {
   //
   const {currentUser} = useAuth();
   //state
   const [login, setLogin] = useState(false);
+  const router = useRouter();
   //look for current user
   useEffect(()=>{
     if(currentUser){
@@ -25,7 +28,8 @@ function RegisterForm() {
         <h3 className='text-2xl font-bold'>Register</h3>
         <p className=" "> Register with your email to receive cupons with discounts and add reviews to our dishes. </p>
       
-        <button className='btn self-center'>Register</button>
+        <button className='btn self-center'
+                onClick={()=>{router.push('/register')}}>Register</button>
       </div>}
     </section>
   )
