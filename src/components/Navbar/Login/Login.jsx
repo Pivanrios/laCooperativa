d921c4bcd00f3d9aafd173789793a8ec3bd91f89
pivanrios <pipr96@gmail.com>
2024-06-06 21:38:26 -0700

    import signOut fnt

--- src/components/Navbar/Login/Login.jsx
+++ src/components/Navbar/Login/Login.jsx
@@ -1,21 +1,31 @@
 'use client'
-
+//firebase functionss
+import { auth } from "@/firebaseconfig";
+import { signOut } from "firebase/auth";
+//next componetns
 import Link from "next/link";
+//react hooks
 import { useState } from "react";
 
 function Login() {
     const [login, setLogin] = useState(true);
-    
+    //
+
+    //logOut button
+    const handleSubmit = ()=>{
+        console.log("logging out")
+        signOut(auth);
+        setLogin(false)
+    }
     return (
         <div>
             {login &&   (<div className="flex flex-col sm:flex-row gap-1">
-                            <button onClick={()=>{setLogin(false)}}>Logout</button>
                             <Link href={"/cupons"}>Cupons</Link>
+                            <button onClick={handleSubmit}>Logout</button>
                         </div>)
             }
             {!login && <Link href={"/login"}>Login</Link>}
         </div>
     )
 }
-
 export default Login
