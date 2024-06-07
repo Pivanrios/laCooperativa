09db30f38ba74af99668716458f41af655182702
pivanrios <pipr96@gmail.com>
2024-06-07 11:14:53 -0700

    add logo

--- src/components/Navbar/CustomerNavbar.jsx
+++ src/components/Navbar/CustomerNavbar.jsx
@@ -9,7 +9,12 @@ function CustomerNavbar() {
   return (
     <header>
         <div className='container flex flex-row  justify-between px-5 py-2 bg-yellow-500'>
-            <span>LaCooperativa</span>
+            <Link href={"/"}><span className='font-bold flex flex-row gap-1 items-center'>
+                              <Image src={'/icons/logo.png'}
+                              width={30}
+                              height={30}
+                              className='hidden sm:list-item rounded-full'/>LaCooperativa</span>
+            </Link>
             <nav className='sm:flex sm:flex-row gap-1 hidden'>
                 <Link href={"/about-us"}
                       className=''>About</Link>
