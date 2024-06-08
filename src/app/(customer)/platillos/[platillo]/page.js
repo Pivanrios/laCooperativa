a91c8a2d63541a9469cd848845d61b038404bdf6
pivanrios <pipr96@gmail.com>
2024-06-08 13:14:30 -0700

    add reviews component

--- src/app/(customer)/platillos/[platillo]/page.js
+++ src/app/(customer)/platillos/[platillo]/page.js
@@ -3,6 +3,7 @@ import React from 'react'
 import ReviewForm from '@/src/components/Reviews/ReviewForm';
 import SingleProduct from '@/src/components/SingleProduct/SingleProduct';
 import DishGallery from '@/src/components/DishSection/DishGallery';
+import ReviewsGallery from '@/src/components/Reviews/ReviewsGallery';
 
 async function Page({params}) {
   const login = false;
@@ -11,9 +12,10 @@ async function Page({params}) {
   return (
     <div className='flex flex-row justify-between px-3 md:px-10 bg-yellow-500 overflow-auto md:mx-14 gap-1'>      
       <div className='flex flex-col bg-yellow-400 p-10 gap-2'>
-        <SingleProduct dishId={params.platillo}/>
-        <ReviewForm productId={params.platillo}/>
+        <SingleProduct dishId={params.platillo}/>        
         <DishGallery title={'Related Dishes'}/>
+        <ReviewForm productId={params.platillo}/>
+        <ReviewsGallery/>
       </div>
     </div>
   )
