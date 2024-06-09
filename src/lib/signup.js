66caf9a4bb6f5f89c0574ea71d6dc04d1e09cf39
pivanrios <pipr96@gmail.com>
2024-06-08 17:53:53 -0700

    add display name

--- src/lib/signup.js
+++ src/lib/signup.js
@@ -1,21 +1,39 @@
 'use server'
 import { auth } from "@/firebaseconfig";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { redirect } from "next/navigation";
 
 
 //create user
 export async function signUpUser(formData){
-    let userCredentials = null
-    const {username, email, password, company, shift} = Object.fromEntries(formData);
-    console.log("entries:", username, email, password, company, shift);
+  //setup variables...
+    let user = null
+    const {username, email, password } = Object.fromEntries(formData);
+    //console.log("entries:", username, email, password);
     try {
-      userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-      //console.log(userCredentials);  
-    } catch (error) {
+      //create user with email address to our firebase authentication service
+      const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+      user = userCredentials.user
+      console.log("succesfully created...", user);  
+      //set username
+      await updateProfile(user, {
+        displayName: username,
+      })
+      console.log("username successfully added...", user.displayName);
+
+    } catch (error) { //customize errors for debbuging
         console.error(error);
+        if(error.code == 'auth/email-already-in-use' ){
+          console.log("Email in use!!!");
+        }else if(error.code == 'auth/weak-password'){
+          console.log('weak password!!!');
+        }else if(error.code == 'auth/invalid-email'){
+          console.log("Invalid email address...");
+        }else{
+          console.log("Something went wrong!!!")
+        }
     }
-    if(userCredentials){
+    if(user){//if user is succesfully added go to SignIn Porces
       redirect('/login')
     }
 };
