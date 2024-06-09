aea68d4c67d01f0ff5a9ddc89cc2bbbac725a5b6
pivanrios <pipr96@gmail.com>
2024-06-08 20:08:05 -0700

    remove cupons

--- src/app/admin/cupons/page.js
+++ src/app/admin/cupons/page.js
@@ -1,15 +1,13 @@
-import DisplayCupons from '@/src/components/DisplayCupons/DisplayCupons';
-import { getCupons } from '@/src/lib/cupons'
+
 import Link from 'next/link';
 import React from 'react'
 
 async function CuponsPage() {
-    const cupons = await getCupons();
+   
   return (
     <div className='mx-20'>
         <h3>Cupons</h3>
         <Link href={'/admin/cupons/new'} className='bg-black text-white px-3 rounded-full'>Add</Link>
-        <DisplayCupons cupons={cupons}/>
     </div>
     
   )
