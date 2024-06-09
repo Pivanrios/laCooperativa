ac3dd08c03c6ee30a9f28c155264f41e8d544476
pivanrios <pipr96@gmail.com>
2024-06-08 20:38:53 -0700

    login required

--- src/components/DisplayCupons/DisplayCupons.jsx
+++ src/components/DisplayCupons/DisplayCupons.jsx
@@ -3,27 +3,38 @@
 import { useEffect, useState } from 'react'
 //components
 import CuponCard from './Cupon/CuponCard'
-//function
+//functions
 import { getCupons } from '@/src/lib/cupons';
+import { useAuth } from '@/src/context/AuthContext';
+import Link from 'next/link';
 
 function DisplayCupons() {
-  //state
+  //state & variables
   const [cupons, setCupons] = useState([]);
+  const {currentUser} = useAuth();
+
   //set cupones
   useEffect(()=>{
     const setData = async ()=>{
       const res = await getCupons();
       setCupons(res);
     }
-    setData();//invoke function
+    setData();
   },[]);
 
   return (
-    <div className='flex flex-col  sm:flex-row gap-1 justify-center items-center'>
-        {cupons.map((cupon)=>(
-            <CuponCard cupon={cupon}/>
-        ))}
-    </div>
+    <>
+      {currentUser && <div className='flex flex-col  sm:flex-row gap-1 justify-center items-center'>
+          {cupons.map((cupon)=>(
+              <CuponCard cupon={cupon}/>
+          ))}
+      </div>}{
+        !currentUser && <div className='p-3 flex flex-col items-center'>
+          <p>Login to see cupons</p>
+          <Link href={'/login'} className='btn'>Login</Link>
+        </div>
+      }
+    </>
   )
 }
 
