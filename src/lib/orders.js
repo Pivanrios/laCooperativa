358c1a0810916bd228fec36ad54e21da1a92a30d
pivanrios <pipr96@gmail.com>
2024-07-19 15:43:38 -0700

    add note and comments

--- src/lib/orders.js
+++ src/lib/orders.js
@@ -1,24 +1,30 @@
 "use server"
+//import CRUD methods
 import { db } from "@/firebaseconfig";
 import { collection, addDoc } from "firebase/firestore";
 
+//asynchronous function using API calls to connect with database
 export async function addOrder(formData){
+    //try and catch bloks para buscar errores.
     try {
         console.log("Setting order..")
-        const {customer, delivery, dish, breakroom, quantity, disc} = Object.fromEntries(formData);
-        
+        //destrocture the values sent.
+        const {customer, delivery, dish, breakroom, quantity, disc, note} = Object.fromEntries(formData);
+        //POST the entries, using await to use the promise 
         const docRef = await addDoc(collection(db, "orders"),{
             customer:customer,
-            delivery:delivery,
+            delivery,
             dish,
             breakroom,
             quantity,
             disc,
-            status:"In-progress"
+            status:"In-progress",
+            note
         })
         console.log("..document added")
     } catch (error) {
         console.error
-        console.log(error)
+        //show the error.
+        console.log("Error:",error)
     }
 }
