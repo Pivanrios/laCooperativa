d11e304c0a0a41f769572bb06477449d2667a12e
pivanrios <pipr96@gmail.com>
2024-07-18 19:32:04 -0700

    call function

--- src/components/DishForm/DishForm.jsx
+++ src/components/DishForm/DishForm.jsx
@@ -1,33 +1,26 @@
-"use client"
-import { useState } from "react"
+import { addProduct } from "@/src/lib/products"
+
 
 
 function DishForm() {
     //getValue from inputs
-    const [name, setName] =  useState("");//string < 30 char
-    const [price,setPrice]= useState(0);//float price to sell in dlls
-    const [cost, setCost] = useState(0);//float in pesos cost of ingredients
-    const [desc, setDesc] = useState("");//text area <100 char
-    const [sides, setSides] = useState([]);//array of sides
-    const [imaUrl, setImgUrl] = useState("");// string 
 
     //create object to send and POST
     //validate data input
     //POST in database
   return (
-    <form action="" className="flex flex-col bg-slate-400 p-5 mx-20 my-2">
+    <form action={addProduct} className="flex flex-col bg-slate-400 p-5 mx-20 my-2">
         <label htmlFor="">Name:
             <input type="text" 
                     name="name" 
                     id="name" 
-                    onChange={(e)=>setName(e.target.value)}
                     required/>
         </label>
         <label htmlFor="">Price:
             <input type="number" name="price" id="price" required/>
         </label>
         <label htmlFor="">Cost of Ingredients:
-            <input type="number" name="ingCosts" id="ingCosts" required />
+            <input type="number" name="costs" id="costs" required />
         </label>
         <label htmlFor="">Description:
             <textarea name="desc" id="desc" required></textarea>
