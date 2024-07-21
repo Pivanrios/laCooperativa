9c13082c67309c776fa77d7e76f2dfc12659c9b3
pivanrios <pipr96@gmail.com>
2024-07-21 00:43:26 -0700

    Side Menu for orders

---
+++ src/components/OrderMenu/OrderMenu.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+
+function OrderMenu() {
+  return (
+    <nav className="flex flex-col p-2 gap-2">
+        <Link href={"/admin/orders/newOrder"}>Add Order</Link>
+        <Link href={"/admin/orders"}>View Orders</Link>
+    </nav>
+  )
+}
+
+export default OrderMenu
