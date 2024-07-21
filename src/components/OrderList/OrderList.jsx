de82f4ca56d62bd67f5cdefb931f1ac722f9b91b
pivanrios <pipr96@gmail.com>
2024-07-21 00:43:13 -0700

    style

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -4,7 +4,8 @@ import { getOrders } from "@/src/lib/orders"
 async function OrderList() {
     const orders = await getOrders();
   return (
-    <section>
+    <section className="flex flex-col w-full">
+        <span className="flex flex-row"><p>Delivery Date</p><p>Customer</p><p>Platillo</p><p>Qty</p></span>
         {orders.map((order)=>(
             <div className="flex flex-row">
             <p>{order.delivery}</p><p>{order.customer}</p><p>{order.dish}</p><p>{order.quantity}</p>
