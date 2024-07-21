baf01287cbd9b3d818936675dc1815cb329a3d82
pivanrios <pipr96@gmail.com>
2024-07-21 01:40:19 -0700

    style

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -5,12 +5,22 @@ async function OrderList() {
     const orders = await getOrders();
   return (
     <section className="flex flex-col w-full">
+      <span className=" justify-between">
+        <button className="btn">Filter</button>
+        <button className="btn">Sort</button>
+      </span>
+      <div>
         <span className="flex flex-row"><p>Delivery Date</p><p>Customer</p><p>Platillo</p><p>Qty</p></span>
-        {orders.map((order)=>(
-            <div className="flex flex-row">
-            <p>{order.delivery}</p><p>{order.customer}</p><p>{order.dish}</p><p>{order.quantity}</p>
-            </div>
-        ))}
+          {orders.map((order)=>(
+              <div className="flex flex-row p-1">
+                <p>{order.orderNum}</p>
+                <p>{order.delivery}</p>
+                <p>{order.customer}</p>
+                <p>{order.dish}</p>
+                <p>{order.quantity}</p>
+              </div>
+          ))}
+      </div>
     </section>
   )
 }
--- src/components/OrderMenu/OrderMenu.jsx
+++ src/components/OrderMenu/OrderMenu.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 function OrderMenu() {
   return (
-    <nav className="flex flex-col p-2 gap-2">
+    <nav className="flex flex-row justify-between md:justify-center md:flex-col p-2 gap-2">
         <Link href={"/admin/orders/newOrder"}>Add Order</Link>
         <Link href={"/admin/orders"}>View Orders</Link>
     </nav>
