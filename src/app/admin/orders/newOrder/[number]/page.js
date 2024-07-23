bae3f9b9700505e3acef24468162298ba25580df
pivanrios <pipr96@gmail.com>
2024-07-23 15:44:35 -0700

    get page params

--- src/app/admin/orders/newOrder/[number]/page.js
+++ src/app/admin/orders/newOrder/[number]/page.js
@@ -1,7 +1,8 @@
 import OrderConfirm from '@/src/components/OrderConfirmation/OrderConfirm'
 import React from 'react'
 
-export default function ConfirmationPage() {
+export default function ConfirmationPage({params}) {
+    console.log("params:",params.number);
   return (
     <div>ConfirmationPage
         <OrderConfirm/>
