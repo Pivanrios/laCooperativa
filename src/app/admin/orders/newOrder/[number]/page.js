4b09c7716b6fddec643806ad177a5bba462d11e8
pivanrios <pipr96@gmail.com>
2024-07-23 15:38:53 -0700

    confirmation components

---
+++ src/app/admin/orders/newOrder/[number]/page.js
@@ -0,0 +1,10 @@
+import OrderConfirm from '@/src/components/OrderConfirmation/OrderConfirm'
+import React from 'react'
+
+export default function ConfirmationPage() {
+  return (
+    <div>ConfirmationPage
+        <OrderConfirm/>
+    </div>
+  )
+}
