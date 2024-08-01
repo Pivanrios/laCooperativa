ea0643a2b1974c58f1ef06d684c318c9c1262783
pivanrios <pipr96@gmail.com>
2024-08-01 16:50:10 -0700

    render order

--- src/components/OrderConfirmation/OrderConfirm.jsx
+++ src/components/OrderConfirmation/OrderConfirm.jsx
@@ -1,9 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
 
-function OrderConfirm() {
+function OrderConfirm({order}) {
   return (
-    <div>
-        Confirmation order #
+    <div className='flex flex-col gap-2 bg-blue-800 m-2 text-white w-80'>
+        <h3>Confirmation Order # {order.orderNum}</h3>
+        <div>
+          <p>Para: {order.customer}</p>
+          <p>Dish: {order.dish}</p>
+          <p>Entrega: {order.delivery}</p>
+          <p>Breakroom: {order.breakroom} </p>
+        </div>
+        <div>
+          <h4>Politicas de entrega</h4>
+          <ol>
+            <li>Manda un Screenshot de esta confirmacion al whatsapp ...</li>
+            <li>Envia comprobante de pago a la cuenta ....</li>
+            <li>Para cancelar avisar con 10 hrs de anticipacion ...</li>
+          </ol>
+          Gracias por su preferencia!
+        </div>
+        <Link href={"/admin/orders"} className='btn'>Confirm</Link>
     </div>
   )
 }
