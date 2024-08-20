d0de78dccb374b5aa9f101b34c1b3fdedfd1223c
pivanrios <pipr96@gmail.com>
2024-08-19 20:19:02 -0700

    style

--- src/components/OrderForm/OrderForCustomer.jsx
+++ src/components/OrderForm/OrderForCustomer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function OrderForCustomer() {
+    //get user info (name, acumulation, shift)
+    //get dishes available
+    
   return (
     <>
-    <form action="">
-        <h3>Order</h3>
+    <form action="" className='flex flex-col gap-1'>
+        <h3>Order for user</h3>
         <label >Dish:
             <input type="text" 
                     name='dish'
@@ -20,6 +23,25 @@ function OrderForCustomer() {
                     name="delivery" 
                     id="delivery" />
         </label>
+        <label htmlFor="breakroom-select">Breakroom:
+            <select name="breakroom" id="breakroom-select">
+                <option value="Main">Main - Norte</option>
+                <option value="Shipdock">Shipdock - Sur</option>
+                <option value="Singles">Singles - Este</option>
+                <option value="Decant">Decant - Oeste</option>
+                <option value="Pick">Pick - 2nd floor</option>
+                <option value="Stow">Stow - 2nd floor</option>
+            </select>
+        </label>
+        <label htmlFor="">Discount:
+            <input type="text"
+                    name="disc"
+                    id="disc" />
+        </label>
+        <label htmlFor="">Nota:
+            <input type="text" name="note"/>
+        </label>
+        <p><strong>Total:</strong></p>
         <input type="button" value="Submit" />
     </form>
     </>
