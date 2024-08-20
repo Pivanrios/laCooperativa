4ff3462a47149397417fa56e6fde62b10497962c
pivanrios <pipr96@gmail.com>
2024-08-19 20:11:00 -0700

    order page

---
+++ src/app/(customer)/order/page.js
@@ -0,0 +1,12 @@
+import OrderForCustomer from '@/src/components/OrderForm/OrderForCustomer'
+import React from 'react'
+
+function OrderPage() {
+  return (
+    <div>OrderPage
+        <OrderForCustomer/>
+    </div>
+  )
+}
+
+export default OrderPage
