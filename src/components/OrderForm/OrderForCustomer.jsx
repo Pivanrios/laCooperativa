bd1b88c9c610ea14b8175eedbee8a2c10773a2c3
pivanrios <pipr96@gmail.com>
2024-08-22 19:58:09 -0700

    calculate total

--- src/components/OrderForm/OrderForCustomer.jsx
+++ src/components/OrderForm/OrderForCustomer.jsx
@@ -7,9 +7,12 @@ import React, { useEffect, useState } from 'react'
 function OrderForCustomer() {
     //get user info (name, acumulation, shift)
     const {currentUser} = useAuth();
-    console.log("current user:", currentUser)
+    //console.log("current user:", currentUser)
     //add states
     const [dishes, setDishes] = useState([]);
+    const [qty, setQty] = useState(0);
+    const [dishSelected, setDishSelected] = useState("");
+    const [total, setTotal] = useState(0);
     //get dishes available
     useEffect(()=>{
         const setData = async ()=>{
@@ -22,7 +25,16 @@ function OrderForCustomer() {
         }
         setData();
     },[])
-    
+    //calculate total
+    useEffect(()=>{
+        //find price
+        if (dishSelected){//check for length
+            const found = dishes.find((dish)=> dish.name === dishSelected)//takes a boolean value
+            //setTotal (price * qty) * disc ++points
+            setTotal(found.price*qty);
+
+        }
+    },[dishSelected, qty])
     
   return (
     <>
@@ -32,10 +44,13 @@ function OrderForCustomer() {
             <input type="text" 
                     name='customer' 
                     id='username' 
-                    value={currentUser.displayName}/>
+                    value={currentUser.displayName}
+                    required/>
         </label>
         <label htmlFor="dish-select">Dish:
-            <select name="dish" id="dish-select" required>
+            <select name="dish" id="dish-select" 
+                    onChange={(e)=>{setDishSelected(e.target.value)}//call use effect 
+                    }required>
                 <option value={""}>------select option------</option>
                 {dishes.map((dish,i)=>(//add key to element
                     <option value={dish.name} key={i}>{dish.name} - ${dish.price}.00</option>
@@ -45,15 +60,21 @@ function OrderForCustomer() {
         <label htmlFor="">Qty:
             <input type="number"
                     name='qty'
-                    id='qty'/>
+                    id='qty'
+                    onChange={(e)=>{// condition value has to be +
+                        setQty(e.target.value);
+                        console.log("value change:",e.target.value)
+                    }}                        
+                    required/>
         </label>
         <label>Date:
             <input type="date" 
                     name="delivery" 
-                    id="delivery" />
+                    id="delivery" 
+                    required/>
         </label>
         <label htmlFor="breakroom-select">Breakroom:
-            <select name="breakroom" id="breakroom-select">
+            <select name="breakroom" id="breakroom-select" required>
                 <option value="Main">Main - Norte</option>
                 <option value="Shipdock">Shipdock - Sur</option>
                 <option value="Singles">Singles - Este</option>
@@ -65,12 +86,13 @@ function OrderForCustomer() {
         <label htmlFor="">Discount:
             <input type="text"
                     name="disc"
-                    id="disc" />
+                    id="disc"/>
         </label>
         <label htmlFor="">Nota:
             <input type="text" name="note"/>
         </label>
-        <p><strong>Total:</strong></p>
+        <p><strong>Total:</strong>{total}</p>
+        <p>points:{total*10}</p>
         <input type="submit" value="Submit" />
     </form>
     </>
