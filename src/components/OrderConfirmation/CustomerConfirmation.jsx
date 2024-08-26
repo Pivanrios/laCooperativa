b56b964b793cacd377820239dcaf719e2d021f3f
pivanrios <pipr96@gmail.com>
2024-08-26 15:37:22 -0700

    confirmation for customer

---
+++ src/components/OrderConfirmation/CustomerConfirmation.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function CustomerConfirmation() {
+  return (
+    <div className='flex flex-col gap-2 bg-blue-800 m-2 text-white w-80'>
+        <h3>Confirmation Order # {order.orderNum}</h3>
+        <div>
+          <p>Para: {order.customer}</p>
+          <p>Dish: {order.dish}</p>
+          <p>Entrega: {order.delivery}</p>
+          <p>Breakroom: {order.breakroom} </p>
+        </div>
+        <div>
+          <h4>Politicas de entrega</h4>
+          <ol>
+            <li>Manda un Screenshot de esta confirmacion al whatsapp ...</li>
+            <li>Envia comprobante de pago a la cuenta ....</li>
+            <li>Para cancelar avisar con 10 hrs de anticipacion ...</li>
+          </ol>
+          Gracias por su preferencia!
+        </div>
+        <Link href={"/profile"} className='btn'>Confirm</Link>
+    </div>
+  )
+}
+
+export default CustomerConfirmation
