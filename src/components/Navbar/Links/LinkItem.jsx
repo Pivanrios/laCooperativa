e5477291c1c55d75e01761d6ffc4382d9a0c00bb
pivanrios <pipr96@gmail.com>
2024-09-03 03:55:08 -0700

    hide page

--- src/components/Navbar/Links/LinkItem.jsx
+++ src/components/Navbar/Links/LinkItem.jsx
@@ -10,7 +10,7 @@ function LinkItem({item, setOpen}) {
     return (
         <Link   href={item.path}
                 className={`hover:bg-blue-700 hover:text-white px-3 rounded-full forced-colors:appearance-auto
-                    ${ path===item.path ? "bg-blue-700 text-white": ""}`}
+                    ${ path===item.path ? "hidden": ""}`}
                 id="link-item"
                 onClick={()=>setOpen(false)}>
         {item.title}
