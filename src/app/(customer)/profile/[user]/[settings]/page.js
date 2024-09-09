89192fca4b36e48f2c0bd1015434e92c763c957d
pivanrios <pipr96@gmail.com>
2024-09-08 20:44:25 -0700

    add settings

--- src/app/(customer)/profile/[user]/[settings]/page.js
+++ src/app/(customer)/profile/[user]/[settings]/page.js
@@ -1,10 +1,12 @@
+import Allergies from '@/src/components/Profile/Navigation/Settings/Allergies'
+import Preferences from '@/src/components/Profile/Navigation/Settings/Preferences'
 import React from 'react'
 
 //there is 3 types of settings [preferences, passord, allergies]
 function SettingsPage({params}) {
   return (
     <>
-    {(params.settings === "preferences")? <p>preferences</p>:<p>other</p>}
+    {(params.settings === "preferences")? <Preferences/>:<Allergies/>}
     </>
   )
 }
