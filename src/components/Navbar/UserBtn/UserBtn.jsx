bc9fbc96ec9ecb6a0f086bed05fc3e6acf8e4a06
pivanrios <pipr96@gmail.com>
2024-09-08 19:33:01 -0700

    dropdown arrow

--- src/components/Navbar/UserBtn/UserBtn.jsx
+++ src/components/Navbar/UserBtn/UserBtn.jsx
@@ -33,7 +33,7 @@ function UserBtn() {
             <>
             <span className=' rounded-full bg-green-700 text-white py-1 px-2'
                     onClick={()=>{(path=="/order" || path=="/profile")? console.log("on ->", path):setOpen(open => !open)}}
-                    >{currentUser.displayName}
+                    > {currentUser.displayName} v
             </span>
             {open && (
                 <div className={`${(path==="/order" || path==="/profile")?"hidden":""} flex flex-col bg-yellow-500 absolute gap-1 right-0`}>
