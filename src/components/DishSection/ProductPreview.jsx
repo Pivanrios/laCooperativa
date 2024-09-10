7489e737847ebcb3776857c6136c4f1690d328f4
pivanrios <pipr96@gmail.com>
2024-09-09 18:50:36 -0700

    next Image

--- src/components/DishSection/ProductPreview.jsx
+++ src/components/DishSection/ProductPreview.jsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 function ProductPreview({d}) {
   return (
       <Link href={`/platillos/${d.id}`}>
         <div id="item-prev" className='container bg-yellow-400 flex  sm:flex-col items-center flex-row gap-1 p-3'>
-            <img alt={d.name} src={d.imagen} className=' w-28'></img>
+            <Image src={d.imagen} alt={d.name} width={20} height={20} />
             <ol className='container flex flex-col justify-between'>
               <h4 className='text-lg font-bold '>{d.name}</h4>
               <p className='self-end'>${d.price}.00</p>
