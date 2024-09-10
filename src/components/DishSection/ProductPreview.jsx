2b84c9e847fa0f0c3af9e90a1bd2c3243a16ba92
pivanrios <pipr96@gmail.com>
2024-09-09 19:20:49 -0700

    look

--- src/components/DishSection/ProductPreview.jsx
+++ src/components/DishSection/ProductPreview.jsx
@@ -6,7 +6,7 @@ function ProductPreview({d}) {
   return (
       <Link href={`/platillos/${d.id}`}>
         <div id="item-prev" className='container bg-yellow-400 flex  sm:flex-col items-center flex-row gap-1 p-3'>
-            <Image src={d.imagen} alt={d.name} width={20} height={20} />
+            <Image src={d.imagen} alt={d.name} width={50} height={60} />
             <ol className='container flex flex-col justify-between'>
               <h4 className='text-lg font-bold '>{d.name}</h4>
               <p className='self-end'>${d.price}.00</p>
--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,17 +1,21 @@
 'use client'
+// APIfunctions
 import { getDish } from '@/src/lib/crud';
-import Image from 'next/image'
+//nextjs components
+import Image from 'next/image';
+//Reac hooks
 import { useEffect, useState } from 'react';
 
 function SingleProduct({dishId}) {
   const [dish, setDish] = useState({});
   //-------
   useEffect(()=>{
+    //sets data from single product
     const setData = async ()=>{
       try {
         console.log("setting data...")
         const res = await getDish(dishId);
-        console.log("setting ingredientes",res.ingredientes);
+        console.log("setting ingredientes");
         setDish(res);
         console.log("succesfully set data")
       } catch (error) {
@@ -22,17 +26,20 @@ function SingleProduct({dishId}) {
   },[]);
   return (
     <div className='flex flex-col items-center '>
+      {dish && (<>
         <h2 className='text-3xl font-bold'>{dish.name}</h2>
         <span className='flex  flex-col md:flex-row gap-2 items-center'>
-            <Image src={dish.imagen} alt={dish.name} width={250} height={400} className=' rounded-xl'/>
+            <Image src={dish.imagen} alt={dish.name} width={250} height={400}/>
             <div className='flex flex-col gap-2'>
                 <p><strong>Description: </strong><br/>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis necessitatibus
                      explicabo eaque exercitationem illo voluptatibus obcaecati fuga odit? Doloribus 
                      officia maiores at dolor earum odio tempore id praesentium suscipit expedita?</p>
-                <p><strong>Complemento:</strong></p>
+                <p><strong>Complemento:</strong>Arroz</p>
                 <p><strong>Price:</strong>${dish.price}.00</p>
             </div>
         </span>
+        </>)
+      }
     </div>
   )
 }
