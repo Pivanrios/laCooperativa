c09140f8ac1cadc07c6c4e4c203f31d499e915bf
pivanrios <pipr96@gmail.com>
2024-09-12 14:35:17 -0700

    add desc

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -31,10 +31,8 @@ function SingleProduct({dishId}) {
         <span className='flex  flex-col md:flex-row gap-2 items-center'>
             <Image src={dish.imagen} alt={dish.name} width={250} height={400}/>
             <div className='flex flex-col gap-2'>
-                <p><strong>Description: </strong><br/>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis necessitatibus
-                     explicabo eaque exercitationem illo voluptatibus obcaecati fuga odit? Doloribus 
-                     officia maiores at dolor earum odio tempore id praesentium suscipit expedita?</p>
-                <p><strong>Complemento:</strong>Arroz</p>
+                <p><strong>Description: </strong><br/>{dish.desc}</p>
+                <p><strong>Complemento:</strong>{dish.sides}</p>
                 <p><strong>Price:</strong>${dish.price}.00</p>
             </div>
         </span>
