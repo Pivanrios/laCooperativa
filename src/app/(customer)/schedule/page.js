01a686b659ee94c58688a42643fd6dc3c8fbc2e6
pivanrios <pipr96@gmail.com>
2025-01-08 15:10:09 -0800

    add calendar

--- src/app/(customer)/schedule/page.js
+++ src/app/(customer)/schedule/page.js
@@ -1,9 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 function SchedulePage() {
   return (
     <div>
         <h3>Schedule</h3>
+        <div>
+          <Image src={'/images/enero2025.png'} height={200} width={2000}/>
+        </div>
     </div>
   )
 }
